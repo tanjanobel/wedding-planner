469b946ce70c24ff639c0463d4e868bcbec1c5c9
Tanja Nobel <tanja@nobel.swiss>
2022-04-18 16:20:20 +0200

    Style login and register page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,6 +4,7 @@ import Section from "../components/Section";
 import axios from "axios";
 import { useNavigate } from "react-router";
 import sprite from "../icons/wedding-planner-sprite.svg";
+import {Link} from "react-router-dom";
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -43,46 +44,66 @@ const Login = () => {
     <>
       <SubHeader title="Anmelden" />
       <Section>
-        <form>
-          <label htmlFor="username">Benutzername</label>
-          <input type="text" id="username" onChange={(e) => setUsername(e.target.value)} />
+        <div className="grid-x grid-margin-x align-top">
+          <div className="card cell small-12 tablet-6">
+            <div className="card__body">
+              <h3 className="card__heading text-center padding-bottom-2">Mit Kundenkonto anmelden</h3>
+              <p className="text-center padding-bottom-1">
+                Du hast schon ein Kundenkonto?<br />
+                Bitte gib deine E-Mail Adresse und dein Passwort ein.
+              </p>
+              <form>
+                <label htmlFor="username">Benutzername</label>
+                <input type="text" id="username" onChange={(e) => setUsername(e.target.value)}/>
 
-          {errors["detail"]?.map((error) => (
-            <div key={error} className="form-error">
-              <svg className="card__status icon small">
-                <use href={sprite + "#exclamation"} />
-              </svg>
-              <span>{error}</span>
+                {errors["detail"]?.map((error) => (
+                  <div key={error} className="form-error">
+                    <svg className="card__status icon small">
+                      <use href={sprite + "#exclamation"}/>
+                    </svg>
+                    <span>{error}</span>
+                  </div>
+                ))}
+
+                {errors["username"]?.map((error) => (
+                  <div key={error} className="form-error">
+                    <svg className="card__status icon small">
+                      <use href={sprite + "#exclamation"}/>
+                    </svg>
+                    <span>{error}</span>
+                  </div>
+                ))}
+
+                <label htmlFor="password">Passwort</label>
+                <input type="password" id="password" onChange={(e) => setPassword(e.target.value)}/>
+
+                {errors["password"]?.map((error) => (
+                  <div key={error} className="form-error">
+                    <svg className="card__status icon small">
+                      <use href={sprite + "#exclamation"}/>
+                    </svg>
+                    <span>{error}</span>
+                  </div>
+                ))}
+
+                <div className="form__footer text-right">
+                  <button onClick={onLogin} type="submit" className="button primary expanded">
+                    Login
+                  </button>
+                </div>
+              </form>
             </div>
-          ))}
-
-          {errors["username"]?.map((error) => (
-            <div key={error} className="form-error">
-              <svg className="card__status icon small">
-                <use href={sprite + "#exclamation"} />
-              </svg>
-              <span>{error}</span>
-            </div>
-          ))}
-
-          <label htmlFor="password">Passwort</label>
-          <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} />
-
-          {errors["password"]?.map((error) => (
-            <div key={error} className="form-error">
-              <svg className="card__status icon small">
-                <use href={sprite + "#exclamation"} />
-              </svg>
-              <span>{error}</span>
-            </div>
-          ))}
-
-          <div className="form__footer text-right">
-            <button onClick={onLogin} type="submit" className="button primary filled">
-              Login
-            </button>
           </div>
-        </form>
+          <div className="card cell small-12 tablet-6">
+            <div className="card__body">
+              <h3 className="card__heading text-center padding-bottom-2">Ich habe noch kein Konto</h3>
+              <p className="text-center padding-bottom-1">
+                Lege jetzt in wenigen Schritten ein Kundenkonto an.
+              </p>
+              <Link to="/register" className="button hollow expanded">Jetzt registrieren</Link>
+            </div>
+          </div>
+        </div>
       </Section>
     </>
   );
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,10 +1,10 @@
-import React, { useState, useContext } from "react";
+import React, {useState, useContext} from "react";
 import SubHeader from "../components/SubHeader";
 import Section from "../components/Section";
 import axios from "axios";
 import sprite from "../icons/wedding-planner-sprite.svg";
 import "react-router";
-import { useNavigate } from "react-router";
+import {useNavigate} from "react-router";
 
 const Register = () => {
   const [username, setUsername] = useState("");
@@ -26,7 +26,7 @@ const Register = () => {
           password,
           password2,
         },
-        { "Content-Type": "application/json" }
+        {"Content-Type": "application/json"}
       )
       .then((response) => {
         navigate("/login");
@@ -39,43 +39,51 @@ const Register = () => {
   return (
     <>
       {/* Flashmessage, bitte einloggen */}
-      <SubHeader title="Registrieren" />
+      <SubHeader title="Registrieren"/>
       <Section>
-        <form>
-          <label htmlFor="username">Benutzername</label>
-
-          <input type="text" id="username" required onChange={(e) => setUsername(e.target.value)} />
-
-          {errors["username"]?.map((error) => (
-            <div key={error} className="form-error">
-              <svg className="card__status icon small">
-                <use href={sprite + "#exclamation"} />
-              </svg>
-              <span>{error}</span>
-            </div>
-          ))}
-
-          <label htmlFor="password">Passwort</label>
-          <input type="password" id="password" required onChange={(e) => setPassword(e.target.value)} />
-
-          {errors["password"]?.map((error) => (
-            <div key={error} className="form-error">
-              <svg className="card__status icon small">
-                <use href={sprite + "#exclamation"} />
-              </svg>
-              <span>{error}</span>
-            </div>
-          ))}
-
-          <label htmlFor="password">Passwort bestätigen</label>
-          <input type="password" id="confirm-password" required onChange={(e) => setPassword2(e.target.value)} />
-
-          <div className="form__footer text-right">
-            <button type="submit" className="button primary filled" onClick={onRegister}>
-              Registrieren
-            </button>
+        <div className="card">
+          <div className="card__body">
+            <h3 className="card__heading text-center padding-bottom-2">Neu bei Wedding Planner? Jetzt einfach
+              registrieren.</h3>
+            <form>
+              <label htmlFor="username">Benutzername (Pflichtfeld)</label>
+              <input type="text" id="username" required onChange={(e) => setUsername(e.target.value)}/>
+              {errors["username"]?.map((error) => (
+                <div key={error} className="form-error">
+                  <svg className="card__status icon small">
+                    <use href={sprite + "#exclamation"}/>
+                  </svg>
+                  <span>{error}</span>
+                </div>
+              ))}
+              <label htmlFor="password">Passwort (Pflichtfeld)</label>
+              <input type="password" id="password" required onChange={(e) => setPassword(e.target.value)}/>
+              {errors["password"]?.map((error) => (
+                <div key={error} className="form-error">
+                  <svg className="card__status icon small">
+                    <use href={sprite + "#exclamation"}/>
+                  </svg>
+                  <span>{error}</span>
+                </div>
+              ))}
+              <label htmlFor="password">Passwort bestätigen (Pflichtfeld)</label>
+              <input type="password" id="confirm-password" required onChange={(e) => setPassword2(e.target.value)}/>
+              <div className="card__text">
+                <ul>
+                  <li>Das Passwort darf nicht zu ähnlich zu Ihren anderen persönlichen Informationen sein.</li>
+                  <li>Das Passwort muss mindestens 8 Zeichen enthalten.</li>
+                  <li>Das Passwort darf nicht allgemein üblich sein.</li>
+                  <li>Das Passwort darf nicht komplett aus Ziffern bestehen.</li>
+                </ul>
+              </div>
+              <div className="form__footer text-right">
+                <button type="submit" className="button primary filled" onClick={onRegister}>
+                  Registrieren
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </Section>
     </>
   );
